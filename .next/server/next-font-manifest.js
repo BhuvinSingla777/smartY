48b6952060101b45200d154ca2cd931fd95d2ac0
bhuvinsingla <bhuvinsingla@gmail.com>
2026-09-08 12:47:03 +0530

    Update .gitignore to remove unnecessary entries and include new deployment configurations.

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/bhuvins/Documents/projects/smartPA/bdg-pods-dashboard 1/bdg-pods-dashboard/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
